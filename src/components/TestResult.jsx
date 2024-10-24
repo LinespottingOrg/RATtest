8a2ca5e7ef0c4e9932833f34fa757da2aabfad92
Joakim Hansen <jocke.hansen@outlook.com>
2024-10-24 09:29:23 +0200

    style: Styled TestResult.jsx

--- src/components/TestResult.jsx
+++ src/components/TestResult.jsx
@@ -23,32 +23,41 @@ function TestResult({ data }) {
 
   return (
     <div className="w-full mx-auto text-center flex flex-col h-full">
-      <div className="m-2 border border-primary">
+      <div className="m-2">
         <RadarChart results={data} />
-        <hr className="border-2 border-primary opacity-50" />
+        <hr className="border-2 border-primary opacity-50 md:mx-12" />
       </div>
       <div className="flex flex-col md:flex-row flex-grow">
-        <div className="m-2 flex-grow border border-primary">
-          <p>Non-conflict</p>
-          <p>{nonConflictResults.awarenessType}</p>
-          <p>{nonConflictResults.animalType}</p>
+        <div className="m-2 flex-grow">
+          <p className="text-xl font-semibold my-2">Non-conflict</p>
+          <hr className="border border-primary mx-8" />
+          <p className="font-semibold my-2">
+            {nonConflictResults.awarenessType}
+          </p>
+          <p className="font-semibold">{nonConflictResults.animalType}</p>
+          <hr className="border border-primary mx-4 my-2" />
           <p>{nonConflictResults.summary}</p>
         </div>
         <div className="hidden md:block w-px h-vh my-5 bg-primary mx-4 blur-sm" />
-        <div className="m-2 flex-grow border border-primary">
-          <p>Conflict</p>
-          <p>{conflictResults.awarenessType}</p>
-          <p>{conflictResults.animalType}</p>
+        <div className="m-2 flex-grow">
+          <p className="text-xl font-semibold my-2">Conflict</p>
+          <hr className="border border-primary mx-8" />
+          <p className="font-semibold my-2">{conflictResults.awarenessType}</p>
+          <p className="font-semibold">{conflictResults.animalType}</p>
+          <hr className="border border-primary mx-4 my-2" />
           <p>{conflictResults.summary}</p>
         </div>
       </div>
       <div className="flex justify-between items-center mx-2">
-        <button className="btn btn-primary" onClick={handleStopTestClick}>
+        <button
+          className="btn btn-primary text-white"
+          onClick={handleStopTestClick}
+        >
           Avsluta test
         </button>
         <div className="flex flex-row justify-end items-center mx-2">
           <p className="mr-4">Psst...vill du ha resultatet på mail?</p>
-          <button className="btn btn-primary">Få Mail</button>
+          <button className="btn btn-primary text-white">Få Mail</button>
         </div>
       </div>
       <div className="flex flex-row justify-between m-2 "></div>
